34cbc9b3796db40f640884e95d32ea66f7416dd7
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-08-21 15:57:18 -0700

    Update

--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 CC=g++
 
 make: termitebot.cpp
-	$(CC) -o /bin/terbot termitebot.cpp
+	$(CC) -o terbot termitebot.cpp
 
 install:
+	cp terbot /bin/terbot
 	cp terbot.1 /usr/share/man/man1/terbot.1
 	gzip /usr/share/man/man1/terbot.1
 
--- termitebot.cpp
+++ termitebot.cpp
@@ -1,38 +1,31 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
 int main() {
-	cout << "████████╗███████╗██████╗ ██████╗  ██████╗ ████████╗\n"
-		 <<	"╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝\n"
-		 <<	"   ██║   █████╗  ██████╔╝██████╔╝██║   ██║   ██║   \n"
-		 <<	"   ██║   ██╔══╝  ██╔══██╗██╔══██╗██║   ██║   ██║   \n"
-		 <<	"   ██║   ███████╗██║  ██║██████╔╝╚██████╔╝   ██║   \n"
-		 <<	"   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝   \n" << endl;
-                                                   
+    std::cout << "████████╗███████╗██████╗ ██████╗  ██████╗ ████████╗\n"
+              << "╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝\n"
+              << "   ██║   █████╗  ██████╔╝██████╔╝██║   ██║   ██║   \n"
+              << "   ██║   ██╔══╝  ██╔══██╗██╔══██╗██║   ██║   ██║   \n"
+              << "   ██║   ███████╗██║  ██║██████╔╝╚██████╔╝   ██║   \n"
+              << "   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝   \n"
+              << "\n";
 
-	cout << "Termitebot: choose theme number\n1:Nature 2:Nord: 3:Ashes 4:Eighties 5:Monokai 6:Zenburn 7:Red 8:90s: ";
-	int userin;
-	cin >> userin;	
-	
-	switch(userin) {
-		case 1 : system("cp -f $HOME/.config/termite/dots/configNatureTheme $HOME/.config/termite/config"); 
-			break;
-		case 2 : system("cp -f $HOME/.config/termite/dots/configNordTheme $HOME/.config/termite/config");
-			break;
-		case 3 : system("cp -f $HOME/.config/termite/dots/configAshesDark $HOME/.config/termite/config");
-			break;
-		case 4 : system("cp -f $HOME/.config/termite/dots/configEightiesTheme $HOME/.config/termite/config");
-			break;
-		case 5 : system("cp -f $HOME/.config/termite/dots/configMonokaiTheme $HOME/.config/termite/config");
-			break;
-		case 6 : system("cp -f $HOME/.config/termite/dots/configZenburnTheme $HOME/.config/termite/config");
-			break;
-		case 7 : system("cp -f $HOME/.config/termite/dots/configRedTheme $HOME/.config/termite/config");
-			break;
-		case 8 : system("cp -f $HOME/.config/termite/dots/config90sTheme $HOME/.config/termite/config");
-			break;
-				
-	}
-  	return 0;
+    std::cout << "Termitebot: choose theme number\n1:Nature 2:Nord: 3:Ashes "
+                 "4:Eighties 5:Monokai 6:Zenburn 7:Red 8:90s: ";
+
+    int userin;
+    std::cin >> userin;
+
+    std::string names[] = {"configNatureTheme",  "configNordTheme",
+                           "configAshesDark",    "configEightiesTheme",
+                           "configMonokaiTheme", "configZenburnTheme",
+                           "configRedTheme"};
+
+    std::string conf_path = "cp -f $HOME/.config/termite/dots/" +
+                            names[userin] + " $HOME/.config/termite/config";
+
+    std::cout << conf_path << "\n";
+    system(conf_path.c_str());
+
+    return 0;
 }
-
